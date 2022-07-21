442f80c519c2b6572b8dba14b8fcdaaccb70a401
NovaPrince <46784613+DarkGomasu@users.noreply.github.com>
2022-07-21 15:37:38 +0530

    many-to-one releationsip add
    One User can upload more than one files

--- CommonStorage/Storage/models.py
+++ CommonStorage/Storage/models.py
@@ -10,19 +10,19 @@ from rest_framework.authtoken.models import Token
 
 class AWS(models.Model):
     file = models.FileField(storage= S3Boto3Storage() )
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     def __str__(self) -> str:
         return str(self.file)
     
 class Azure(models.Model):
     file = models.FileField(storage = AzureStorage() )
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     def __str__(self) -> str:
         return str(self.file)
     
 class GCloud(models.Model):
     file = models.FileField(storage= GoogleCloudStorage)
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
     def __str__(self) -> str:
         return str(self.file)
     
--- CommonStorage/Storage/views.py
+++ CommonStorage/Storage/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import api_view,authentication_classes,permission
 from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication,SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
+from django.core import serializers
 
 from Storage.models import AWS as dbAws, Azure as dbAzure, GCloud as dbGCloud
 from CommonStorage.settings import AWS_ACCESS_KEY_ID,AWS_SECRET_ACCESS_KEY, AZURE_ACCOUNT_KEY, AZURE_ACCOUNT_NAME, GS_ACCESS_KEY_ID,GS_SECRET_ACCESS_KEY,GS_BUCKET_NAME
@@ -19,9 +20,12 @@ def AWS(request):
         return Response("File Saved")
     if(len(AWS_SECRET_ACCESS_KEY) == 0 or len(AWS_SECRET_ACCESS_KEY) == 0 ):
         return Response("{PLease Provide AWS Credentails in Settings  to be able to upload to S3 Storage}")
-    aws = dbAws.objects.filter(user=request.user)
     
-    return Response(aws[0].file.url)
+    aws = dbAws.objects.filter(user=request.user)
+    data = []
+    for f in aws:
+        data.append(f.file.url)
+    return Response(data)
 
 @api_view(['POST'])
 @authentication_classes([TokenAuthentication,SessionAuthentication])
@@ -35,8 +39,10 @@ def Azure(request):
     if(len(AZURE_ACCOUNT_NAME) == 0 or len(AZURE_ACCOUNT_KEY) == 0 ):
         return Response("{PLease Provide Azure Credentails in Settings  to be able to upload to Blob Storage}")
     blob = dbAzure.objects.filter(user=request.user)
-    
-    return Response(blob[0].file.url)
+    data = []
+    for f in blob:
+        data.append(f.file.url)
+    return Response(data)
 
 @api_view(['POST'])
 @authentication_classes([TokenAuthentication,SessionAuthentication])
@@ -52,7 +58,9 @@ def GCloud(request):
     if(len(GS_ACCESS_KEY_ID) == 0 or len(GS_BUCKET_NAME) == 0 or len(GS_SECRET_ACCESS_KEY) == 0 ):
         return Response("{PLease Provide Google Credentails in Settings to be able to upload to Cloud Storage}")
     gcloud = dbGCloud.objects.filter(user=request.user)
-    
-    return Response(gcloud[0].file.url)
+    data = []
+    for f in gcloud:
+        data.append(f.file.url)
+    return Response(data)
     
 #     return Response(aws[0].file.url)
